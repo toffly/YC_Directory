8596d3f1c462b9ed60c0adbf298c02feb1736304
toffly <toffly@outlook.com>
2025-09-21 13:31:29 +0700

    project commit

---
+++ components/Ping.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Ping = () => {
+  return (
+    <div className="relative">
+      <div className="absolute -left-4 top-1">
+        <span className="flex size-[11px]">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary-500 opacity-75"></span>
+          <span className="relative inline-flex size-[11px] rounded-full bg-primary-500"></span>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Ping;
